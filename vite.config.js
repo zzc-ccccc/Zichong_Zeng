8adcd57b5d7f09678159c62ca6bede0f5648083a
zzc-ccccc <709191484@qq.com>
2025-02-06 17:33:15 +1100

    修改index.html 重定向

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,14 @@ export default defineConfig({
   plugins: [vue()],
   base: process.env.NODE_ENV === "production"
   ? '/Zichong_Zeng/'
-  : '/'
+  : '/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: './index.html',
+        404: './404.html'
+      }
+    }
+  }
 
 })
