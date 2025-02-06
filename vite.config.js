9a06b850bfaeee9dccbe62bfe51e6376528830e9
zzc-ccccc <709191484@qq.com>
2025-02-06 16:52:59 +1100

    add github action

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: process.env.NODE_ENV === "production"
+  ? 'Zichong_Zeng'
+  : '/'
+
 })
